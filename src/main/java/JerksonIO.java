6d4cb8cec7f66f42a0b7db5d312a5cc0120a058e
louie <louiecasula@gmail.com>
2023-12-12 17:06:46 -0500

    made prices descending order, removed empty values

--- src/main/java/JerksonIO.java
+++ src/main/java/JerksonIO.java
@@ -2,9 +2,7 @@ import org.apache.commons.io.IOUtils;
 
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.HashSet;
+import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -62,35 +60,35 @@ public class JerksonIO {
         int appleCount = gl.getApples().size();
         gl.countError();
         int errorCount = gl.getError();
-        HashMap<String, Integer> milkPrices = countMilkPrices();
-        HashMap<String, Integer> breadPrices = countBreadPrices();
-        HashMap<String, Integer> cookiesPrices = countCookiesPrices();
-        HashMap<String, Integer> applesPrices = countApplesPrices();
+        TreeMap<Double, Integer> milkPrices = countMilkPrices();
+        TreeMap<Double, Integer> breadPrices = countBreadPrices();
+        TreeMap<Double, Integer> cookiesPrices = countCookiesPrices();
+        TreeMap<Double, Integer> applesPrices = countApplesPrices();
         StringBuilder sb = new StringBuilder();
         sb.append("name:    Milk 		 seen: ").append(milkCount).append(" times\n");
         sb.append("============= \t \t =============\n");
-        for (String key: milkPrices.keySet()){
+        for (Double key: milkPrices.keySet()){
             sb.append("Price: \t ").append(key).append("		 seen: ").append(milkPrices.get(key)).append(" times\n");
             sb.append("-------------\t\t -------------\n");
         }
         sb.append("\n");
-        sb.append("name:    Bread		 seen: ").append(breadCount).append(" times\n");
+        sb.append("name:   Bread		 seen: ").append(breadCount).append(" times\n");
         sb.append("============= \t \t =============\n");
-        for (String key: breadPrices.keySet()){
+        for (Double key: breadPrices.keySet()){
             sb.append("Price: \t ").append(key).append("		 seen: ").append(breadPrices.get(key)).append(" times\n");
             sb.append("-------------\t\t -------------\n");
         }
         sb.append("\n");
-        sb.append("name:    Cookies     seen: ").append(cookieCount).append(" times\n");
+        sb.append("name: Cookies        seen: ").append(cookieCount).append(" times\n");
         sb.append("============= \t \t =============\n");
-        for (String key: cookiesPrices.keySet()){
+        for (Double key: cookiesPrices.keySet()){
             sb.append("Price: \t ").append(key).append("		 seen: ").append(cookiesPrices.get(key)).append(" times\n");
             sb.append("-------------\t\t -------------\n");
         }
         sb.append("\n");
-        sb.append("name:    Apples		seen: ").append(appleCount).append(" times\n");
+        sb.append("name:  Apples		 seen: ").append(appleCount).append(" times\n");
         sb.append("============= \t \t =============\n");
-        for (String key: applesPrices.keySet()){
+        for (Double key: applesPrices.keySet()){
             sb.append("Price: \t ").append(key).append("		 seen: ").append(applesPrices.get(key)).append(" times\n");
             sb.append("-------------\t\t -------------\n");
         }
@@ -99,87 +97,96 @@ public class JerksonIO {
         return sb.toString();
     }
 
-    public static HashMap<String, Integer> countMilkPrices(){
-        ArrayList<String> prices = new ArrayList<>();
+    public static TreeMap<Double, Integer> countMilkPrices(){
+        ArrayList<Double> prices = new ArrayList<>();
         for (GroceryItem item : gl.getMilk()){
-            prices.add(item.getPrice());
+            if (item.getPrice().isEmpty()){
+                continue;
+            }
+            else {
+                prices.add(Double.valueOf(item.getPrice()));
+            }
         }
-        HashSet<String> difPrice = new HashSet<>(prices);
-        HashMap<String, Integer> allPrices = new HashMap<>();
-        for (String price: difPrice){
+        HashSet<Double> difPrice = new HashSet<>(prices);
+        TreeMap<Double, Integer> allPrices = new TreeMap<>(Comparator.reverseOrder());
+        for (Double price: difPrice){
             allPrices.put(price, 0);
         }
-        for (String key: allPrices.keySet()){
-            for (String price: prices){
+        for (Double key: allPrices.keySet()){
+            for (Double price: prices){
                 if(key.equals(price)){
                     int count = allPrices.get(key);
                     allPrices.put(key, count + 1);
                 }
             }
         }
+
         return allPrices;
     }
 
-    public static HashMap<String, Integer> countBreadPrices(){
-        ArrayList<String> prices = new ArrayList<>();
-        for (GroceryItem item : gl.getBread()){
-            prices.add(item.getPrice());
+    public static TreeMap<Double, Integer> countBreadPrices() {
+        ArrayList<Double> prices = new ArrayList<>();
+        for (GroceryItem item : gl.getBread()) {
+            prices.add(Double.valueOf(item.getPrice()));
         }
-        HashSet<String> difPrice = new HashSet<>(prices);
-        HashMap<String, Integer> allPrices = new HashMap<>();
-        for (String price: difPrice){
+        HashSet<Double> difPrice = new HashSet<>(prices);
+        TreeMap<Double, Integer> allPrices = new TreeMap<>(Comparator.reverseOrder());
+        for (Double price : difPrice) {
             allPrices.put(price, 0);
         }
-        for (String key: allPrices.keySet()){
-            for (String price: prices){
-                if(key.equals(price)){
+        for (Double key : allPrices.keySet()) {
+            for (Double price : prices) {
+                if (key.equals(price)) {
                     int count = allPrices.get(key);
                     allPrices.put(key, count + 1);
                 }
             }
         }
+
         return allPrices;
     }
 
-    public static HashMap<String, Integer> countCookiesPrices(){
-        ArrayList<String> prices = new ArrayList<>();
-        for (GroceryItem item : gl.getCookies()){
-            prices.add(item.getPrice());
+    public static TreeMap<Double, Integer> countCookiesPrices() {
+        ArrayList<Double> prices = new ArrayList<>();
+        for (GroceryItem item : gl.getCookies()) {
+            prices.add(Double.valueOf(item.getPrice()));
         }
-        HashSet<String> difPrice = new HashSet<>(prices);
-        HashMap<String, Integer> allPrices = new HashMap<>();
-        for (String price: difPrice){
+        HashSet<Double> difPrice = new HashSet<>(prices);
+        TreeMap<Double, Integer> allPrices = new TreeMap<>(Comparator.reverseOrder());
+        for (Double price : difPrice) {
             allPrices.put(price, 0);
         }
-        for (String key: allPrices.keySet()){
-            for (String price: prices){
-                if(key.equals(price)){
+        for (Double key : allPrices.keySet()) {
+            for (Double price : prices) {
+                if (key.equals(price)) {
                     int count = allPrices.get(key);
                     allPrices.put(key, count + 1);
                 }
             }
         }
+
         return allPrices;
     }
 
-    public static HashMap<String, Integer> countApplesPrices(){
-        ArrayList<String> prices = new ArrayList<>();
-        for (GroceryItem item : gl.getApples()){
-            prices.add(item.getPrice());
+    public static TreeMap<Double, Integer> countApplesPrices() {
+        ArrayList<Double> prices = new ArrayList<>();
+        for (GroceryItem item : gl.getApples()) {
+            prices.add(Double.valueOf(item.getPrice()));
         }
-        HashSet<String> difPrice = new HashSet<>(prices);
-        HashMap<String, Integer> allPrices = new HashMap<>();
-        for (String price: difPrice){
+        HashSet<Double> difPrice = new HashSet<>(prices);
+        TreeMap<Double, Integer> allPrices = new TreeMap<>(Comparator.reverseOrder());
+        for (Double price : difPrice) {
             allPrices.put(price, 0);
         }
-        for (String key: allPrices.keySet()){
-            for (String price: prices){
-                if(key.equals(price)){
+        for (Double key : allPrices.keySet()) {
+            for (Double price : prices) {
+                if (key.equals(price)) {
                     int count = allPrices.get(key);
                     allPrices.put(key, count + 1);
                 }
             }
         }
+
         return allPrices;
     }
 
